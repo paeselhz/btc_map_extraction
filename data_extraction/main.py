0a6a021c1511db4ddd9e2de1768b3e80a360dd11
Luis Paese <lhzpaese@gmail.com>
2024-07-17 21:02:14 -0400

    applied code_style make

--- data_extraction/main.py
+++ data_extraction/main.py
@@ -1,5 +1,6 @@
-import pandas as pd
 import json
+
+import pandas as pd
 import requests
 
 
@@ -9,7 +10,6 @@ class BTCMapExtraction:
         self.node_base_url = "https://api.btcmap.org/v2/elements/node:"
         self.elements_base_url = "https://static.btcmap.org/api/v2/elements.json"
 
-
     def request_node(self, node):
         node_req = requests.get(f"{self.node_base_url}{node}")
 
@@ -17,9 +17,9 @@ class BTCMapExtraction:
             node_dict = json.loads(node_req.content.decode())
         else:
             raise RuntimeError
-        
+
         return node_dict
-    
+
     def request_elements(self):
 
         elements_req = requests.get(self.elements_base_url)
@@ -28,35 +28,35 @@ class BTCMapExtraction:
             elements_dict = json.loads(elements_req.content.decode())
         else:
             raise RuntimeError
-        
+
         return elements_dict
-    
+
     def individual_df(self, node_dict):
-        
+
         individual_keys = {
-            "type": node_dict.get('osm_json').get('type'),
-            "id": node_dict.get('osm_json').get('id'),
-            "lat": node_dict.get('osm_json').get('lat'),
-            "lon": node_dict.get('osm_json').get('lon'),
-            "user": node_dict.get('osm_json').get('user'),
-            'created_at': node_dict.get('created_at'),
-            'updated_at': node_dict.get('updated_at'),
-            'deleted_at': node_dict.get('deleted_at')
+            "type": node_dict.get("osm_json").get("type"),
+            "id": node_dict.get("osm_json").get("id"),
+            "lat": node_dict.get("osm_json").get("lat"),
+            "lon": node_dict.get("osm_json").get("lon"),
+            "user": node_dict.get("osm_json").get("user"),
+            "created_at": node_dict.get("created_at"),
+            "updated_at": node_dict.get("updated_at"),
+            "deleted_at": node_dict.get("deleted_at"),
         }
 
-        keys_df = pd.DataFrame.from_dict([individual_keys], orient='columns')
+        keys_df = pd.DataFrame.from_dict([individual_keys], orient="columns")
 
-        osm_json_tags = node_dict.get('osm_json').get('tags')
+        osm_json_tags = node_dict.get("osm_json").get("tags")
 
         if osm_json_tags is not None:
 
-            tags_df = pd.DataFrame.from_dict([osm_json_tags], orient='columns')
+            tags_df = pd.DataFrame.from_dict([osm_json_tags], orient="columns")
 
-            tags_df.columns = 'tags_' + tags_df.columns
+            tags_df.columns = "tags_" + tags_df.columns
         else:
             tags_df = pd.DataFrame()
-        
-        ret_df = pd.concat([keys_df, tags_df], axis = 1)
+
+        ret_df = pd.concat([keys_df, tags_df], axis=1)
 
         ret_df.dropna(axis=1, inplace=True)
 
@@ -66,7 +66,6 @@ class BTCMapExtraction:
 
         list_dfs = [self.individual_df(x) for x in list_elements]
 
-        ret_df = pd.concat(list_dfs, axis = 0, ignore_index=True)
+        ret_df = pd.concat(list_dfs, axis=0, ignore_index=True)
 
         return ret_df
-
--- main.py
+++ main.py
@@ -1,23 +1,26 @@
 import pandas as pd
+
 from data_extraction.main import BTCMapExtraction
 
 btcmap = BTCMapExtraction()
 
 elements_dict = btcmap.request_elements()
 
-n_runs = int(elements_dict.__len__()/1000) + 1
+n_runs = int(elements_dict.__len__() / 1000) + 1
 
 all_elements_df = pd.DataFrame()
 
 for i in range(0, n_runs):
     print(f"Range from {i*1000} to {(i+1)*1000}")
-    elements_inter = btcmap.elements_df(elements_dict[i*1000:(i+1) * 1000])
+    elements_inter = btcmap.elements_df(elements_dict[i * 1000 : (i + 1) * 1000])
 
-    all_elements_df = pd.concat([all_elements_df, elements_inter], axis = 0, ignore_index=True)
+    all_elements_df = pd.concat(
+        [all_elements_df, elements_inter], axis=0, ignore_index=True
+    )
 
 # all_elements_df
 
-all_elements_df.to_csv('all_elements_df.csv', index=False)
+all_elements_df.to_csv("all_elements_df.csv", index=False)
 
 # individual_node = btcmap.request_node(node=11089175599)
 
